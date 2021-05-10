0a51164d50dd390e23e57a3d9df3483b2b6e1204
Fatima Fakih <fatima.fakih@hotmail.com.au>
2021-05-10 16:34:59 +1000

    added badge routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -6,7 +6,7 @@ const db = require("../models");
 module.exports = (app) => {
     //GET ALL SCORES
     app.get("/api/all", (req, res) => {
-        Model.Score.findAll({}).then((results) => res.json(results));
+        db.Score.findAll({}).then((results) => res.json(results));
     });
     // SPECIFIC SCORE
     app.get("/api/:score", (req, res) => {
@@ -46,12 +46,55 @@ module.exports = (app) => {
             },
             {
                 where: {
-                    id: req.body.id,
+                    email: req.body.email,
                 },
                 //change to email as that's whats independent
             }
         ).then((dbScore) => res.json(dbScore));
     });
+
+
+    app.get('/api/badge', (req, res) => {
+        // findAll returns all entries for a table when used with no options
+        db.Badge.findAll({}).then((dbBadge) => res.json(dbBadge));
+    });
+
+    // POST route for saving a new todo
+    app.post('/api/badge', (req, res) => {
+        const badge = req.body;
+        db.Badge.create({
+            badge_name: req.body.badge_name,
+            score: req.body.score,
+            image: req.body.score
+        }).then((dbBadge) => res.json(dbBadge);
+    });
+
+    // DELETE route for deleting todos using the ID (req.params.id)
+    app.delete('/api/badge/:id', (req, res) => {
+        // We just have to specify which todo we want to destroy with "where"
+        db.Badge.destroy({
+            where: {
+                id: req.params.id,
+            },
+        }).then((dbBadge) => res.json(dbBadge);
+    });
+
+    // PUT route for updating todos. We can get the updated todo data from req.body
+    app.put('/api/badge', (req, res) => {
+        db.Badge.update(
+            {
+                badge_name: req.body.badge_name,
+                score: req.body.score,
+                image: req.body.score
+            },
+            {
+                where: {
+                    id: req.body.id,
+                },
+            }
+        ).then((dbBadge) => res.json(dbBadge));
+    });
+
 
     //badge to be 
     // if statement for score to be checked along with existing badge//
