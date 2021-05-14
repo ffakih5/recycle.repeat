2c0789c31d74fe9130ba684297ac37858f198483
Fatima Fakih <fatima.fakih@hotmail.com.au>
2021-05-14 18:14:05 +1000

    json including installed dependencies; creating home and login react pages

--- client/src/App.js
+++ client/src/App.js
@@ -9,52 +9,11 @@ import NavBar from "./components/NavBar";
 
 //import { useAuth0 } from '@auth0/auth0-react';
 function App() {
-    /*const {
-      isLoading, 
-      isAuthenticated,
-      error,
-      user,
-      loginWithRedirect,
-      logout,
-    } = useAuth0();
-    */
-   const isLoading = false;
-   const isAuthenticated = true; 
-   const error = false;
-   const user = {
-     name: "foobar",
-     email: "foobar@foo.com"
-   };
-   const logout = "logout"
-   const loginWithRedirect = () => {
-    return true;
-   }
-    if (isLoading) { 
-      return <div>Loading...</div>;
-    }
-    if (error) {
-      console.log(error);
-      return <div>Oops... {error.message}</div>;
-    }
-   function loadResults () {
-     console.log("loadResults");
-   }
-    if (isAuthenticated) {
       return (
         <div>
-          Hello {user.name}{' '}
-          <form>
-            <input type = "text"></input>
-            <button onClick={loadResults}>Load Results</button>
-          </form>
-          <button onClick={() => logout({ returnTo: window.location.origin })}>
-            Log out
-          </button>
+          <Home />
         </div>
       );
-    } else {
-      return <button onClick={loginWithRedirect}>Log in</button>;
-    }
   }
 
 export default App;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import { Text, StyleSheet } from "react-native";
 
+function Home(props) {
+    const [titleText, setTitleText] = useState("Recyle Repeat");
+    const bodyText = useState("Thank you for choosing to help out mother earth by recycling waste");
+
+    const onPressTitle = () => {
+        setTitleText("Recycle Repeat [pressed]");
+      };
+    return (
+        <div>
+           return (
+    <Text style={styles.baseText}>
+      <Text style={styles.titleText} onPress={onPressTitle}>
+        {titleText}
+        {"\n"}
+        {"\n"}
+      </Text>
+      <Text numberOfLines={5}>{bodyText}</Text>
+    </Text>
+  );
+
+
+        </div>
+    )
+}
+const styles = StyleSheet.create({
+    baseText: {
+      fontFamily: "Cochin"
+    },
+    titleText: {
+      fontSize: 20,
+      fontWeight: "bold"
+    }
+  });
+  
 
 export default Home;
