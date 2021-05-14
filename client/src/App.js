df48f10ef3b09ce41eb60ba9805701a3228db6bb
Fatima Fakih <fatima.fakih@hotmail.com.au>
2021-05-14 19:28:38 +1000

    moved navbar import before home

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import NavBar from "./components/NavBar";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Record from "./pages/Record";
 import Results from "./pages/Results";
 import Share from "./pages/Share";
-import NavBar from "./components/NavBar";
+
 
 //import { useAuth0 } from '@auth0/auth0-react';
 function App() {
