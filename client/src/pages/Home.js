34f1edd4ff20cb74b4697d0ed58877091dfcca16
Fatima Fakih <fatima.fakih@hotmail.com.au>
2021-05-14 16:12:49 +1000

    working on react componenets; error from fb button

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,4 @@
 import React from "react";
 
 
-export default 
+export default Home;
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,10 +1,24 @@
 import React from "react";
+import NavBar from "../components/NavBar";
 
-<div className="fb-login-button" data-width="" data-size="large" data-button-type="continue_with" data-layout="default" data-auto-logout-link="false" data-use-continue-as="false"></div>
-<div>
+function Login(props) {
+}
+return (
+  <div>
+
+  </div>
+)
+
+export default Login;
+
+
+/*<div className="fb-login-button" data-width="" data-size="large" data-button-type="continue_with" data-layout="default" data-auto-logout-link="false" data-use-continue-as="false">
 <fb:login-button 
   scope="public_profile,email"
   onlogin="checkLoginState();">
 </fb:login-button>
 
 </div>
+*/
+
+
