a05b47908a16baa8a4df34a95a4aaf89e4847e2d
Fatima Fakih <fatima.fakih@hotmail.com.au>
2021-05-17 19:27:20 +1000

    corrected line 21

--- routes/users.js
+++ routes/users.js
@@ -7,11 +7,19 @@ const router = express.Router();
 // send react 
 router.get('/user', secured(), function (req, res, next) {
     const { _raw, _json, ...userProfile } = req.user; //get current badge above
-    /*res.render('user', {
+    res.render('user', {
         userProfile: JSON.stringify(userProfile, null, 2), //current badge.
         title: 'Profile page'
     });
-    res.sendFile(path.join(__dirname,;*/
+    /*router.use(function(req, res) {
+  console.log("no route hit")
+  res.sendFile(path.join(__dirname, "../client/public/index.html"));
+});
+*/
+
+    router.get('/user', (req, res) => {
+        res.send(req.user)
+    });
 });
 
 module.exports = router;
