e778126ad32fe578a2c58664f7f954adc1ea7c31
Fatima Fakih <fatima.fakih@hotmail.com.au>
2021-05-17 22:23:27 +1000

    testing routes

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 ![Badge](https://img.shields.io/badge/recycle--repeat-BeKindToMotherNature-green)
 
-A simple and easy to navigate application designed with the intention to inspire and instill and encourage good habits among children 
+A simple and easy to navigate application designed with the intention to inspire and instill and encourage good habits among children. 
--- routes/users.js
+++ routes/users.js
@@ -2,23 +2,23 @@
 const express = require('express');
 const secured = require('../middleware/secured');
 const router = express.Router();
+const path = require('path');
 
 /* GET user profile. */
 // send react 
 router.get('/user', secured(), function (req, res, next) {
     const { _raw, _json, ...userProfile } = req.user; //get current badge above
-    res.render('user', {
+    /*res.render('user', {
         userProfile: JSON.stringify(userProfile, null, 2), //current badge.
         title: 'Profile page'
-    });
-    router.use(function (req, res) {
-        console.log("no route hit")
-        res.sendFile(path.join(__dirname, "../client/public/index.html"));
-    });
+    });*/
 
-    router.get('/user-id', (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/public/index.html"));
+
+    router.get('/user', (req, res) => {
         res.send(req.user)
     });
+    console.log(req.user);
 });
 
 module.exports = router;
